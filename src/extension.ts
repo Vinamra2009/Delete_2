b19d79169c86a3deab4e0847f230714e3b0eefdd
Vinamra Sulgante <vsulgante@deloitte.com>
2023-08-02 15:03:06 +0530

    hook  test

--- src/extension.ts
+++ src/extension.ts
@@ -8,11 +8,14 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sample-ext-2" is now active!');
+	
+	
+	console.log ('Congratulations, your extension "sample-ext-2" is now active!') ;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
+	// eslint-disable-next-line prefer-const
 	let disposable = vscode.commands.registerCommand('sample-ext-2.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
@@ -22,6 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
-//New comment
 // This method is called when your extension is deactivated
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 export function deactivate() {}
